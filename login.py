3d53281f35a4509acac8ddd133f9b618ae5b7763
wbjxxzx <wbjxxzx@163.com>
2018-01-17 15:49:53 +0800

    complete login

--- login.py
+++ login.py
@@ -3,71 +3,6 @@
 """
 登陆流程:
 获取验证码 -> 校验验证码 -> 校验帐号密码 -> 登陆成功
-
-GET  验证码: https://kyfw.12306.cn/passport/captcha/captcha-image
-参数:
-login_site:E
-module:login
-rand:sjrand
-0.3638160110014952
-
-POST 验证码校验: https://kyfw.12306.cn/passport/captcha/captcha-check
-校验结果: result_message
-    验证码校验成功  ret_code:4
-    验证码校验失败  ret_code:5
-    验证码已经过期  ret_code:7
-    验证码校验失败,信息为空  ret_code:8
-参数:
-answer:42,38,253,34,54,116
-login_site:E
-rand:sjrand
-
-8张图片模型 67*67 间隔为 5px
------------------
-|png|png|png|png|
------------------
-|png|png|png|png|
------------------
-各图片左上角对应坐标
-5,5  77,5  149,5  221,5
-5,77 77,77 149,77 221,77
-
-POST 帐号密码: https://kyfw.12306.cn/passport/web/login
-返回: {"result_message":"登录名不存在。","result_code":1}
-成功后返回: {"result_message":"登录成功","result_code":0,"uamtk":"VEqbgNHudMa-N1meoVuv0x9eL-MXBCP5gat1t
-0"}
-login 验证结果
-    登录名不存在  result_code:1
-    登录成功 result_code:0
-
-登陆成功后:
-POST userLogin: https://kyfw.12306.cn/otn/login/userLogin
-request Headers [Referer:https://kyfw.12306.cn/otn/login/init]
-参数:
-_json_att:
-
-POST uamtk: https://kyfw.12306.cn/passport/web/auth/uamtk
-request Headers [Referer:https://kyfw.12306.cn/otn/passport?redirect=/otn/login/userLogin]
-参数:
-appid:otn
-返回: json
-{"result_message":"验证通过","result_code":0,"apptk":null,"newapptk":"OJ-LJPr1VyujjZUEsekzuT-Ll42Fb5LKbct1t0"}
-
-POST uamauthclient: https://kyfw.12306.cn/otn/uamauthclient
-request Headers [Referer:https://kyfw.12306.cn/otn/passport?redirect=/otn/login/userLogin]
-参数:
-tk:UV5AG4BN2u9zna6l5QHTQws_Ft2THVB4kot2t0
-返回: {"result_code":2,"result_message":"uamtk票据内容为空"}
-成功后返回:
-apptk=FUMjBieik2NEM2kc01s6uxls94vbBW4brwt1t0
-result_code=0
-result_message=验证通过
-username=张三
-
-GET userLogin: https://kyfw.12306.cn/otn/login/userLogin
-request Headers [Referer:https://kyfw.12306.cn/otn/passport?redirect=/otn/login/userLogin]
-
-
 """
 
 from urllib import request, parse
@@ -75,7 +10,7 @@ from http import cookiejar
 from collections import namedtuple
 from PIL import Image
 import json, random, ssl, tempfile, os, time, sys, re
-import logging;logging.basicConfig(level=logging.DEBUG)
+from myLogger import logger
 
 try:
     import myInfo
@@ -88,18 +23,28 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 class MyHTTPRedirectHandler(request.HTTPRedirectHandler, request.HTTPCookieProcessor):
     def http_error_301(self, req, fp, code, msg, httpmsg):
-        logging.debug(req)
-        logging.debug(httpmsg)
+        logger.debug(req)
+        logger.debug(httpmsg)
         return request.HTTPRedirectHandler.http_error_301(self, req, fp, code, msg, httpmsg)
     def http_error_302(self, req, fp, code, msg, httpmsg):
-        logging.debug(req)
-        logging.debug(httpmsg)
+        logger.debug(req)
+        logger.debug(httpmsg)
         return request.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, httpmsg)
 
 class My12306(object):
     _cj = cookiejar.LWPCookieJar()
     _opener = request.build_opener(MyHTTPRedirectHandler(_cj))
-
+    ''' 
+    8张图片模型 67*67 间隔为 5px
+    -----------------
+    |png|png|png|png|
+    -----------------
+    |png|png|png|png|
+    -----------------
+    各图片左上角对应坐标
+    5,5  77,5  149,5  221,5
+    5,77 77,77 149,77 221,77 
+    '''
     def __init__(self):
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) \
@@ -136,8 +81,8 @@ class My12306(object):
     def doGET(self, url, data=None, headers=None):
         if data is not None: 
             url = url + "?" + data
-        logging.debug("cookie: [{}]".format(self._cj))
-        logging.info("GET: [{}]".format(url))
+        logger.debug("cookie: [{}]".format(self._cj))
+        logger.info("GET: [{}]".format(url))
         if headers is None: headers = self.headers
         req = request.Request(url, headers=headers)
         with My12306._opener.open(req) as f:
@@ -146,11 +91,12 @@ class My12306(object):
     def doPOST(self, url, data, headers=None):
         if headers is None: headers = self.headers
         req = request.Request(url, headers=headers)
-        logging.debug("cookie: [{}]".format(self._cj))
-        logging.info("POST: [{}] ,data [{}]".format(url, data))
+        logger.debug("cookie: [{}]".format(self._cj))
+        logger.info("POST: [{}] ,data [{}]".format(url, data))
         with My12306._opener.open(req, data=data.encode("utf-8")) as f:
             return f.status, f.read()
     
+    # 一切从这里开始
     def getStartPage(self):
         self.doGET(self._startPage)
 
@@ -163,24 +109,17 @@ class My12306(object):
                 random.random(): ""
             }
             retCode, retData = self.doGET(self._captchaImg, parse.urlencode(captchaImgData))
-            ''' url = self._captchaImg + "?" + parse.urlencode(captchaImgData)
-            req = request.Request(url)
-            with request.urlopen(req) as f:
-                logging.debug("getheaders:")
-                for k, v in f.getheaders():
-                    logging.debug("[{}]:[{}]".format(k, v))
-                retCode, retData = f.status, f.read() '''
-            logging.info("retCode:{}".format(retCode))
+            logger.info("retCode:{}".format(retCode))
             
             if retCode == 200:
                 png = os.path.join(self._tmp, "captcha.png")
-                logging.info("保存图片为:{}".format(png))
+                logger.info("保存图片为:{}".format(png))
                 with open(png, "wb") as fw:
                     fw.write(retData)
             try:
                 img = Image.open(png)
             except:
-                logging.info("未获取到验证码，重新获取...")
+                logger.info("未获取到验证码，重新获取...")
                 continue
             yield img.show()
 
@@ -200,7 +139,18 @@ class My12306(object):
             posList.append(random.randint(self._posMap[pos][1].a, self._posMap[pos][1].b)) 
         return ",".join([str(x) for x in posList])
         
-
+    '''
+    POST 验证码校验: https://kyfw.12306.cn/passport/captcha/captcha-check
+    校验结果: result_message
+        验证码校验成功  ret_code:4
+        验证码校验失败  ret_code:5
+        验证码已经过期  ret_code:7
+        验证码校验失败,信息为空  ret_code:8
+    参数:
+    answer:42,38,253,34,54,116
+    login_site:E
+    rand:sjrand
+    '''
     def checkCaptcha(self):
         ok = False
         g = self.getCaptchaImg()
@@ -213,13 +163,13 @@ class My12306(object):
                 "login_site": "E",
                 "rand": "sjrand",
             }
-            logging.debug(captchaForm)
+            logger.debug(captchaForm)
             retCode, retData = self.doPOST(self._captchaChk, parse.urlencode(captchaForm))
-            logging.info("retCode:[{}]".format(retCode))
+            logger.info("retCode:[{}]".format(retCode))
             if retCode == 200:
                 try:
                     result = json.loads(retData.decode("utf-8"))
-                    logging.info("retData:[{}]".format(result))
+                    logger.info("retData:[{}]".format(result))
                     if result["result_code"] == "4":
                         ok = True
                         break
@@ -227,65 +177,89 @@ class My12306(object):
                     pass
             g.send(None)
         g.close()
-        logging.info("验证码校验成功, 尝试登陆")
-
+        logger.info("验证码校验成功, 尝试登陆")
+    '''
+    POST 帐号密码: https://kyfw.12306.cn/passport/web/login
+    返回: {"result_message":"登录名不存在。","result_code":1}
+    成功后返回: {"result_message":"登录成功","result_code":0,"uamtk":"VEqbgNHudMa-N1meoVuv0x9eL-MXBCP5gat1t0"}
+    login 验证结果
+        登录名不存在  result_code:1
+        登录成功 result_code:0 
+    '''
     def checkUser(self, user="12306", passwd="123456"):
         loginForm = {
             "username": user,
             "password": passwd,
             "appid": "otn",
         }
-        logging.debug(loginForm)
+        logger.info("发送登录数据: {}".format(loginForm))
         ok = False
         while not ok:
             retCode, retData = self.doPOST(self._loginURL, parse.urlencode(loginForm))
             if retCode == 200:
-                logging.debug(retData.decode("utf-8"))
+                logger.debug(retData.decode("utf-8"))
                 try:
                     result = json.loads(retData.decode("utf-8"))
                     if result["result_code"] == 0:
-                        logging.info("用户验证通过, 登录成功")
+                        logger.info("用户名和密码验证通过")
                         self.tokenParams["tk"] = result["uamtk"]
                     else:
                         return result["result_message"]
                     ok = True
                 except json.decoder.JSONDecodeError as e:
-                    logging.error("json parse failed: {}".format(e))
+                    logger.debug("json parse failed: {}".format(e))
                     time.sleep(2)
                 except UnicodeDecodeError as e:
-                    logging.error("[{}], [{}]".format(e, retData))
+                    logger.debug("[{}]".format(e))
+    '''
+    POST uamtk: https://kyfw.12306.cn/passport/web/auth/uamtk
+    request Headers [Referer:https://kyfw.12306.cn/otn/passport?redirect=/otn/login/userLogin]
+    参数: appid:otn
+    返回: {"result_message":"验证通过","result_code":0,"apptk":null,"newapptk":"OJ-LJPr1VyujjZUEsekzuT-Ll42Fb5LKbct1t0"}
 
+    POST uamauthclient: https://kyfw.12306.cn/otn/uamauthclient
+    request Headers [Referer:https://kyfw.12306.cn/otn/passport?redirect=/otn/login/userLogin]
+    参数:(上一步的 uamtk)  tk:UV5AG4BN2u9zna6l5QHTQws_Ft2THVB4kot2t0
+    返回: {"result_code":2,"result_message":"uamtk票据内容为空"}
+    成功后返回:
+    apptk=FUMjBieik2NEM2kc01s6uxls94vbBW4brwt1t0
+    result_code=0
+    result_message=验证通过
+    username=张三
+    '''
     def doLogin(self):
         headers = self.headers
         headers["Content-Type"] = "application/x-www-form-urlencoded"
         retCode, retData = self.doPOST("https://kyfw.12306.cn/otn/login/userLogin", 
             parse.urlencode({"_json_att": ""}), headers=headers)
-        # logging.debug("retCode:[{}], retData:[{}]".format(retCode, retData.decode("utf-8", errors="ignore")))
+        # logger.debug("retCode:[{}], retData:[{}]".format(retCode, retData.decode("utf-8", errors="ignore")))
 
+        logger.info("尝试 uamtk 验证...")
         headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
         retCode, retData = self.doPOST("https://kyfw.12306.cn/passport/web/auth/uamtk", 
             parse.urlencode({"appid": "otn"}), headers=headers)
         if retCode == 200:
-            logging.debug("retCode:[{}], retData:[{}]".format(retCode, retData.decode("utf-8")))
+            logger.debug("retCode:[{}], retData:[{}]".format(retCode, retData.decode("utf-8")))
             try:
                 result = json.loads(retData.decode("utf-8"))
                 if result["result_code"] == 0:
-                    logging.info("uamtk验证通过")
+                    logger.info("uamtk 验证通过")
                     self.tokenParams["newapptk"] = result["newapptk"]
                 else:
                     return result["result_message"]
             except:
                 pass
 
+        logger.info("尝试 uamauthclient 验证...")
         retCode, retData = self.doPOST("https://kyfw.12306.cn/otn/uamauthclient", 
             parse.urlencode({"tk": self.tokenParams["newapptk"]}), headers=headers)
         if retCode == 200:
-            logging.debug("retCode:[{}], retData:[{}]".format(retCode, retData.decode("utf-8")))
+            logger.debug("retCode:[{}], retData:[{}]".format(retCode, retData.decode("utf-8")))
             try:
                 result = json.loads(retData.decode("utf-8"))
                 if result["result_code"] == 0:
-                    logging.info("uamauthclient验证通过")
-                    logging.info("成功登录12306 {} 可以买票了".format(result["username"]))
+                    logger.info("uamauthclient 验证通过")
+                    logger.info("成功登录12306, 用户名[{}], 可以买票了".format(result["username"]))
                 else:
                     return result["result_message"]
             except:
@@ -301,7 +275,7 @@ class My12306(object):
         
         headers["Referer"] = "https://kyfw.12306.cn/otn/index/initMy12306"
         retCode, retData = self.doGET("https://kyfw.12306.cn/otn/leftTicket/init", headers=headers)
-        logging.debug("retCode:[{}], retData:[{}]".format(retCode, retData.decode("utf-8")))
+        logger.debug("retCode:[{}], retData:[{}]".format(retCode, retData.decode("utf-8")))
         with open("initMy12306.html", "wb") as fw:
             fw.write(retData)
 
@@ -312,8 +286,6 @@ class My12306(object):
                 "rand": "randp",
                 random.random(): "",
             }), headers=headers)
-
-
 
 if __name__ == "__main__":
     my12306 = My12306()
