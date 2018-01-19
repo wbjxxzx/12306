9fe5d1fa0a5e625c5893f1f614ea2ecb857bff79
wbjxxzx <wbjxxzx@163.com>
2018-01-19 08:43:50 +0800

    add trainIdxMap

---
+++ trainIdxMap.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+"""
+[0] tf%2B3IVkDJ7uFD4smUZpouHqCUnYHQroOGXrhxEaCG9ALaJ1BDRKHiPDEM8Mn%2F4dKC0R3i52ReHd8%0AFRhAkoB7np86Y8P4WbTa1GovFeB5f
+ViSVDkFp7RpQCcQ0OXDsDFa5f8v4gYpXNJsj12%2B0hSplzpA%0A3vOzbJtUkFRQRSxFEjIVMDl1t6vGGEL5y%2BJ3ex%2Fe0yZRGa21MlZz1MAOI9U3
+HtVZYKtwhvV0g7vZ%0AsiUvF%2Fvcj6aATm3geFxj2d7hfSTy9R2%2FbSFK3L0%3D
+[1] 预订
+[2] 6c000G11400F
+[3] G1140
+[4] IZQ
+[5] WHN
+[6] IZQ
+[7] WHN
+[8] 18:43
+[9] 23:01
+[10] 04:18
+[11] Y
+[12] BfwS4mAMpcQ4E8qbPZCZ8nSKPYw%2BhRsIfsjw%2FdXg%2F74o2MTB1XgTwcuOSt0%3D
+[13] 20180129
+[14] 3
+[15] Q9
+[16] 01
+[17] 08
+[18] 1
+[19] 0
+[20]
+[21]
+[22]
+[23]
+[24]
+[25]
+[26] 无
+[27]
+[28]
+[29]
+[30] 有
+[31] 16
+[32] 3
+[33]
+[34] O090M0O0
+[35] O9MO
+[36] 0
+"""
+"""
+车次 3  发车时间 8 到达 9 历时 10 发车日期 13 是否可预订 11
+商务 32 一等 31 二等 30
+无座 26 软卧 23 硬卧 28 硬座 29
+"""
+
+secretStr = 0
+train_no = 2
+stationTrainCode = 3
+fromStationTelecode = 6
+toStationTelecode = 7
+bookable = 11
+train_date = 13
+train_location = 15
+seat_1 = 31
+seat_2 = 30
+seat_normal = 29
+seat_bed = 28
+seat_softbed = 23
